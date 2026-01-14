880d7a0d0a95b6b02e5b88b1d8d9b83642e282f7
Lennart Heinrich <lennart.heinrich@edu.fhdw.de>
2026-01-14 14:22:40 +0100

    remove unused import

--- src/main/java/de/fhdw/knn/network/Network.java
+++ src/main/java/de/fhdw/knn/network/Network.java
@@ -7,7 +7,6 @@ import de.fhdw.knn.network.layer.DenseLayer;
 import de.fhdw.knn.network.layer.InputLayer;
 import de.fhdw.knn.network.connection.Connection;
 import de.fhdw.knn.network.neuron.AbstractDenseNeuron;
-import de.fhdw.knn.network.neuron.SuperNeuron;
 
 import java.util.Random;
 import java.util.stream.IntStream;
